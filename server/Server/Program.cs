4be42fc302c7fb2c7f301696384706fe7747e6ea
Yordan <yordan.j.martinez@gmail.com>
2025-07-05 15:03:35 -0400

    Fixing DockerFile 7

--- server/Server/Program.cs
+++ server/Server/Program.cs
@@ -89,11 +89,15 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 //know what kind of error 
 app.UseStatusCodePages();
